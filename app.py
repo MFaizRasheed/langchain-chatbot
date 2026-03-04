697db5df706533599efa7bec962042a1be1982ab
Muhammad Faiz Rasheed <mfaizrasheed776@gmail.com>
2026-03-04 10:51:53 +0500

    🔧 Refactor: rename main.py to app.py

---
+++ app.py
@@ -0,0 +1,10 @@
+from langchain_ollama import ChatOllama
+
+llm = ChatOllama(
+    model="minimax-m2.5:cloud",
+    temperature=0.7,
+    # other params...
+)
+
+response = llm.invoke("What is Rag?")
+print(response.content)
