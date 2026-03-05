428eef7324212dc2f82f8e4fc56701a3d3b720b4
Muhammad Faiz Rasheed <mfaizrasheed776@gmail.com>
2026-03-05 08:24:34 +0500

    update

--- app.py
+++ app.py
@@ -1,10 +1,20 @@
 from langchain_ollama import ChatOllama
+from langchain_core.messages import SystemMessage,HumanMessage#,AIMessage
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.output_parsers import StrOutputParser
 
 llm = ChatOllama(
     model="minimax-m2.5:cloud",
-    temperature=0.7,
-    # other params...
+    temperature = 0.7,
+    #other parameters
 )
 
-response = llm.invoke("What is Rag?")
-print(response.content)
+promt = ChatPromptTemplate.from_messages([
+    ("system","You are a helpful AI assistant. "),
+    ("human","{question}")
+])
+chain = promt | llm | StrOutputParser()
+# response = chain.invoke({"question":"What is RAG?"})
+# print(response)
+for chunk in chain.stream({"question":"What is RAG?"}):
+    print(chunk, end="", flush=True)
